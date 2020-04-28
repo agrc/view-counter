97db08740c6f7e6aca2ca0820fb77776e3d5d2fd
steveoh <sgourley@utah.gov>
2020-04-27 19:09:20 -0600

    feat: async/await and new datastore api updates

--- index.js
+++ index.js
@@ -1,61 +1,72 @@
-const Datastore = require('@google-cloud/datastore');
+const { Datastore } = require("@google-cloud/datastore");
 
-const VIEW_KIND = 'view';
+const VIEW_KIND = "view";
 const BAD_REQUEST = 400;
 const SERVER_ERROR = 500;
 
-const datastore = new Datastore({ projectId: 'utahkoopserver' });
+const datastore = new Datastore();
 
-exports.viewcounter = (request, response) => {
-    // CORS headers
-    response.set('Access-Control-Allow-Origin', '*');
-    response.set('Access-Control-Allow-Methods', 'GET');
+exports.viewcounter = async (request, response) => {
+  // CORS headers
+  response.set("Access-Control-Allow-Origin", "*");
+  response.set("Access-Control-Allow-Methods", "GET");
 
-    const referrer = request.get('referrer');
-    console.log('referrer', referrer);
+  const referrer = request.get("referrer");
+  console.log("referrer", referrer);
 
-    if (!referrer) {
-        response.status(BAD_REQUEST).json({
-            message: 'referrer is undefined!'
-        });
+  if (!referrer) {
+    response.status(BAD_REQUEST).json({
+      message: "referrer is undefined!",
+    });
+  }
+
+  if (referrer.indexOf("deploy-preview") > -1) {
+    return response.json({ preview: true });
+  }
+  if (referrer.indexOf("gis.utah.gov") === -1) {
+    return response.json({ skip: true });
+  }
+
+  const key = datastore.key([VIEW_KIND, referrer]);
+  const [entity] = await datastore.get(key);
+  console.log(entity);
+
+  if (!entity) {
+    const data = { count: 1 };
+    try {
+      await datastore.save({
+        key,
+        data,
+      });
+    } catch (error) {
+      return response.status(SERVER_ERROR).json({
+        message: "error creating record",
+        error: error || "",
+      });
     }
 
-    const key = datastore.key([VIEW_KIND, referrer]);
+    console.log("record created successfully");
 
-    datastore.get(key, (err, entity) => {
-        if (entity) {
-            entity.count++;
+    response.json({ count: entity.count });
+  }
 
-            datastore.save({
-                key,
-                data: { count: entity.count }
-            }, (error) => {
-                if (error) {
-                    response.status(SERVER_ERROR).json({
-                        message: 'error updating record',
-                        error
-                    });
-                } else {
-                    console.log('record updated successfully');
-                    response.json({ count: entity.count });
-                }
-            });
-        } else {
-            const data = { count: 1 };
-            datastore.save({
-                key,
-                data
-            }, (error) => {
-                if (error) {
-                    response.status(SERVER_ERROR).json({
-                        message: 'error creating record',
-                        error
-                    });
-                } else {
-                    console.log('record created successfully');
-                    response.json(data);
-                }
-            });
-        }
+  console.log(entity.count);
+  entity.count++;
+  console.log(entity.count);
+
+  try {
+    await datastore.save({
+      key,
+      data: { count: entity.count },
     });
+  } catch (error) {
+    return response.status(SERVER_ERROR).json({
+      message: "error updating record",
+      error: error || "",
+    });
+  }
+
+  console.log("record updated successfully");
+
+  return response.json({ count: entity.count });
 };
